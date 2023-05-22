b3c3a5891c82c93c01ab0124062c359a01f667b7
manuelasimes <manuela.simes10@gmail.com>
2023-05-22 15:06:27 -0300

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './componentes/Navbar';
 import {Home} from './paginas/Home';
+import {Login} from "./paginas/Login";
 function App() {
 
-  return (
-    <div className="App">
+        let component
+        switch (window.location.pathname) {
+            case '/':
+            component = <Home />
+            break
+            case '/home':
+            component = <Home />
+            break
+            case '/login':
+            component = <Login />
+                break
+    }
+    return (
+        <div className="App">
         <Navbar />
-        <Home />
-    </div>
+            {component}
+        </div>
   );
+
 }
 
 export default App;
--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,3 +1,3 @@
-export default function Login() {
+export function Login() {
     return <h1>Log In</h1>
 }
